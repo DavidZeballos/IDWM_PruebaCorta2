24d2da6c2a27b2798a65aea06d1af43872dbbb4c
DavidZeballos <david.zeballos@alumnos.ucn.cl>
2024-05-29 11:18:51 -0400

    feat: Base de la evaluación

---
+++ chairs-dotnet7-api/Chair.cs
@@ -0,0 +1,16 @@
+namespace chairs_dotnet7_api
+{
+    public class Chair
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }  = string.Empty;
+        public string Tipo { get; set; }  = string.Empty;
+        public string Material { get; set; }  = string.Empty;
+        public string Color { get; set; }  = string.Empty;
+        public int Altura { get; set; }  
+        public int Anchura { get; set; } 
+        public int Profundidad { get; set; }  
+        public int Precio { get; set; } 
+        public int Stock { get; set; }
+    }
+}
---
+++ chairs-dotnet7-api/DataContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace chairs_dotnet7_api
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options) { }
+
+        public DbSet<Chair> GetChairs { get; set; }
+    }
+}
